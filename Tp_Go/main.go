b5fc1b5d308286208fa4641fd54a4dd6ab779aac
JoMar <contact@jomar.fr>
2019-02-01 13:49:00 +0100

    Update : Rangement des fichiers

---
+++ Tp_Go/main.go
@@ -0,0 +1,274 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/kataras/go-sessions"
+)
+
+var db *sql.DB
+var err error
+
+type user struct {
+	ID        int
+	Username  string
+	FirstName string
+	LastName  string
+	Password  string
+}
+
+func connectDB() {
+	db, err = sql.Open("mysql", "go:mypassword@tcp(127.0.0.1)/go_db")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func routes() {
+	http.HandleFunc("/", home)
+	http.HandleFunc("/register", register)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/wrongLogins", wrongLogins)
+	http.HandleFunc("/logout", logout)
+	http.HandleFunc("/users", listUsers)
+	http.HandleFunc("/tweets", listTweets)
+	http.HandleFunc("/newTweet", newTweet)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+}
+
+func main() {
+	connectDB()
+	routes()
+
+	defer db.Close()
+
+	fmt.Println("Server running on port :8100")
+	http.ListenAndServe(":8100", nil)
+}
+
+func checkErr(w http.ResponseWriter, r *http.Request, err error) bool {
+	if err != nil {
+
+		fmt.Println(r.Host + r.URL.Path)
+
+		http.Redirect(w, r, r.Host+r.URL.Path, 301)
+		return false
+	}
+
+	return true
+}
+
+func queryUser(username string) user {
+	var users = user{}
+	err = db.QueryRow(`
+		SELECT id, 
+		username, 
+		first_name, 
+		last_name, 
+		password 
+		FROM users WHERE username=?
+		`, username).
+		Scan(
+			&users.ID,
+			&users.Username,
+			&users.FirstName,
+			&users.LastName,
+			&users.Password,
+		)
+	return users
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	session := sessions.Start(w, r)
+	if len(session.GetString("username")) == 0 {
+		http.Redirect(w, r, "/login", 301)
+	}
+
+	var data = map[string]string{
+		"username": session.GetString("username"),
+		"message":  "Welcome to the Go !",
+	}
+	var t, err = template.ParseFiles("views/home.html")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	t.Execute(w, data)
+
+	return
+}
+
+func newTweet(w http.ResponseWriter, r *http.Request) {
+	var id int
+
+	session := sessions.Start(w, r)
+	if len(session.GetString("username")) == 0 {
+		http.Redirect(w, r, "/login", 301)
+	}
+
+	if r.Method != "POST" {
+		http.ServeFile(w, r, "views/home.html")
+		return
+	}
+
+	tweet := r.FormValue("tweet")
+	username := session.GetString("username")
+
+	userID, err := db.Query("SELECT id FROM users WHERE username = ?", username)
+
+	if err == nil {
+		for userID.Next() {
+			err = userID.Scan(&id)
+		}
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	stmt, err := db.Prepare("INSERT INTO tweets SET userID=?, tweet=?")
+	if err == nil {
+		_, err := stmt.Exec(id, tweet)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+
+	http.Redirect(w, r, "/home", http.StatusSeeOther)
+	return
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.ServeFile(w, r, "views/register.html")
+		return
+	}
+
+	username := r.FormValue("username")
+	firstName := r.FormValue("first_name")
+	lastName := r.FormValue("last_name")
+	password := r.FormValue("password")
+
+	users := queryUser(username)
+
+	if (user{}) == users {
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+		if len(hashedPassword) != 0 && checkErr(w, r, err) {
+			stmt, err := db.Prepare("INSERT INTO users SET username=?, password=?, first_name=?, last_name=?")
+			if err == nil {
+				_, err := stmt.Exec(&username, &hashedPassword, &firstName, &lastName)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+
+				http.Redirect(w, r, "/login", http.StatusSeeOther)
+				return
+			}
+		}
+	} else {
+		http.Redirect(w, r, "/register", 302)
+	}
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	session := sessions.Start(w, r)
+	if len(session.GetString("username")) != 0 && checkErr(w, r, err) {
+		http.Redirect(w, r, "/", 302)
+	}
+	if r.Method != "POST" {
+		http.ServeFile(w, r, "views/login.html")
+		return
+	}
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	users := queryUser(username)
+
+	var passwordTest = bcrypt.CompareHashAndPassword([]byte(users.Password), []byte(password))
+
+	if passwordTest == nil {
+		session := sessions.Start(w, r)
+		session.Set("username", users.Username)
+		session.Set("name", users.FirstName)
+		http.Redirect(w, r, "/", 302)
+	} else {
+		http.Redirect(w, r, "/wrongLogins", 302)
+		//http.Redirect(w, r, "/login", 302)
+	}
+
+}
+
+func listUsers(w http.ResponseWriter, r *http.Request) {
+	var username string
+	var user []string
+
+	userID, err := db.Query("SELECT username FROM users")
+
+	if err == nil {
+		for userID.Next() {
+			err = userID.Scan(&username)
+			user = append(user, username)
+		}
+		t, _ := template.ParseFiles("views/users.html")
+		t.Execute(w, &user)
+	}
+}
+
+func listTweets(w http.ResponseWriter, r *http.Request) {
+	users, _ := r.URL.Query()["user"]
+	var id int
+	var tweet string
+	var userTweet []string
+
+	userID, err := db.Query("SELECT id FROM users WHERE username = ?", users[0])
+
+	if err == nil {
+		for userID.Next() {
+			err = userID.Scan(&id)
+		}
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	tweets, err := db.Query("SELECT tweet FROM tweets where userID = ?", id)
+	if err == nil {
+		for tweets.Next() {
+			err = tweets.Scan(&tweet)
+			userTweet = append(userTweet, tweet)
+		}
+
+		t, _ := template.ParseFiles("views/tweets.html")
+		t.Execute(w, &userTweet)
+	}
+	return
+
+}
+
+func wrongLogins(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "views/wrongLogins.html")
+	return
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	session := sessions.Start(w, r)
+	session.Clear()
+	sessions.Destroy(w, r)
+	http.Redirect(w, r, "/", 302)
+}
---
+++ Tp_Go/tpGo.sql
@@ -0,0 +1,81 @@
+-- MySQL dump 10.13  Distrib 5.7.25, for Linux (x86_64)
+--
+-- Host: localhost    Database: go_db
+-- ------------------------------------------------------
+-- Server version	5.7.25-0ubuntu0.18.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tweets`
+--
+
+DROP TABLE IF EXISTS `tweets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tweets` (
+  `tweetID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `userID` int(11) DEFAULT NULL,
+  `tweet` mediumtext,
+  PRIMARY KEY (`tweetID`),
+  KEY `fk_userID_ID` (`userID`),
+  CONSTRAINT `fk_userID_ID` FOREIGN KEY (`userID`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tweets`
+--
+
+LOCK TABLES `tweets` WRITE;
+/*!40000 ALTER TABLE `tweets` DISABLE KEYS */;
+INSERT INTO `tweets` VALUES (21,7,'Vous êtes trop naze !'),(22,8,''),(23,8,'test'),(24,8,'');
+/*!40000 ALTER TABLE `tweets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `first_name` varchar(200) NOT NULL,
+  `last_name` varchar(200) NOT NULL,
+  `password` varchar(120) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (7,'Jomar','Joshua','Martinelle','$2a$10$vOInmVgkTJYgv/zPqrj1fujnruLCxf29bS7dtYWgnVVdq/kcADiGC'),(8,'william','','','$2a$10$wTjVB/a/KnGt.skolnaK4.7GsA3jJOColWbXbgl9TNJH2gWy9eGGy'),(9,'<script>alert(0)</script>','','','$2a$10$hldnzMy4vhT.dkmyO3kLH.jsgiuBJWt2YqEYTBjuUO32N4nTXiKz.');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-02-01 13:41:21
